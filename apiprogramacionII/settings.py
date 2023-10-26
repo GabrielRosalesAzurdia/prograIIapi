64d1c2daa146df01f57680fe43c2815f024053cb
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-10-26 06:18:01 -0600

    agregando origins

--- apiprogramacionII/settings.py
+++ apiprogramacionII/settings.py
@@ -156,5 +156,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:8080",
-"http://127.0.0.1:9000"
+"http://127.0.0.1:9000",
+"http://127.0.0.1:5173",
+"http://127.0.0.1:5500",
 ]
